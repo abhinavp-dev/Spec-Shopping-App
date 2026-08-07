5e1f5bede954976a4e17f367db05d6c120b18426
soorajt <soorajt@opentrends.net>
2026-08-07 11:51:21 +0530

    Initial Commit

---
+++ Spec-Shopping/Spec_ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spec_ShoppingApp.swift
+//  Spec-Shopping
+//
+//  Created by Abhinav        on 07/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Spec_ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
